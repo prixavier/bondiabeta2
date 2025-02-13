ec9c1fddb96dbbd5b8199b3ad7a304022d25cfa5
Priscilla Xavier <priscillaxavier1@gmail.com>
2025-02-12 22:07:14 -0500

    Initial Commit

---
+++ bondia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bondia
+//
+//  Created by Priscilla Xavier on 2/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bondia/bondiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  bondiaApp.swift
+//  bondia
+//
+//  Created by Priscilla Xavier on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct bondiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
